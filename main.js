37add8505e91f5e2687977493c701edaaaed21cb
kauemenezessilva-cmyk <kaue.menezes.silva@escola.pr.gov.br>
2026-08-14 13:45:01 +0000

    Atualizações em main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+// Seleção de Elementos
+const mario = document.querySelector ('.mario');
+const restarButton = document.querySelector ('.restarButton');
+const startButton = document.querySelector ('.startButton');
+const pipe = document.querySelector ('.pipe');
+const gameOver = document.querySelector ('.gameOver');
+
+
+
+//Definição de Audio
+const audioStart = new Audio('./sound/audio_theme.mp3')
+const audiogameover = new Audio('./sound/audio_gameover.mp3')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
